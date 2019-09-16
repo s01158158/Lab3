a3be887528e4065a9e900818593e4e76e7892ff5
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-09-15 23:11:24 -0400

    Update testCrypto.py

--- testCrypto.py
+++ testCrypto.py
@@ -1,1 +1,15 @@
+Crypto("How is it going today?")
+'o si on oa?Hwi tgigtdy'
+>>> CryptoDecrypt('o si on oa?Hwi tgigtdy')
+'How is it going today?'
+>>> Crypto("Good how about you?")
+'odhwaotyuGo o bu o?'
+>>> CryptoDecrypt('odhwaotyuGo o bu o?')
+'Good how about you?'
+>>> CryptoDecrypt("What is the weather supposed to be tomorrow?")
+'pWphoaste di st ot hbee  wteoamtohrerro ws?u'
+>>> Crypto('pWphoaste di st ot hbee  wteoamtohrerro ws?u')
+'What is the weather supposed to be tomorrow?'
 
+
+# I couldn't get the input function to work with my code unless everything was typed out separately for some reason.
